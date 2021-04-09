6cad9f5f667eaae6f88b946647cc1890c39a0876
ChiaraM96 <krmn.21@outlook.com>
2021-04-09 16:19:36 +0200

    GROUP_06 Final Delivery

--- GROUP_06.cydsn/InterruptRoutines.h
+++ GROUP_06.cydsn/InterruptRoutines.h
@@ -1,4 +1,5 @@
 /* ========================================
+ *
 Electronic Technologies and Biosensors Laboratory
 Academic Year 2020/2021 - II Semester
 Assignment 02
@@ -9,6 +10,7 @@ Header file for the interrupt service routines
 -   Custom_UART_RX_ISR: the UART triggers an interrupt as soon as a single byte is received
 -   Custom_TIMER_SET_ISR: timeout for user input during inbetween states
 
+ *
  * ========================================
 */
 
